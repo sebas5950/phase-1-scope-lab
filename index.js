959300cd27fc4531aa6eba8545838e3fb2024cf3
Alex Guanipatin <sebas5950@gmail.com>
2022-05-17 12:51:25 -0400

    commit tree 4614ac40017056304dc56d118d22a19315269b1f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = "joshman";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "notjosh";
+}
